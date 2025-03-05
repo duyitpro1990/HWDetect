f57a58c7ce08c494747709bf3cfbdc23436c5e19
duyitpro1990 <duyitpro90@gmail.com>
2025-03-05 10:26:24 +0700

    test

--- app.py
+++ app.py
@@ -28,3 +28,5 @@ def predict():
     image = request.form.get('imgBase64')
     result = ai.detectHW(image)
     return str(result)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
